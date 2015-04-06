c9dbf2af2fbe159bfdbc3ad2923556c8225c38dc
kighie <kighie@gmail.com>
2015-04-06 21:09:04 +0900

    script

---
+++ Formula/src/main/java/kr/simula/formula/script/statement/IfStatement.java
@@ -0,0 +1,52 @@
+/* ******************************************************************************
+ * Copyright (c) 2012 IkChan Kwon kighie@gmail.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package kr.simula.formula.script.statement;
+
+import kr.simula.formula.core.Context;
+import kr.simula.formula.core.Gettable;
+
+/**
+ * <pre>
+ * </pre>
+ * @author Ikchan Kwon
+ *
+ */
+public class IfStatement extends AbstractStatement {
+
+	private Gettable<Boolean> condition;
+	
+	/**
+	 * @param condition
+	 */
+	public IfStatement(Gettable<Boolean> condition) {
+		super();
+		this.condition = condition;
+	}
+
+	@Override
+	public String getExpression() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void eval(Context context) {
+		if( condition.get(context) ){
+			
+		}
+	}
+
+	
+}
